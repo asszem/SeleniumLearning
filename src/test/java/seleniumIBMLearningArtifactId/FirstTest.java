d909c4ff0efadd64fd480a47a0b0ea524d3f4474
Andras Olah <andras.antal.olah@hu.ibm.com>
2017-10-03 11:06:43 +0200

    Add simple Google page test

---
+++ src/test/java/seleniumIBMLearningArtifactId/FirstTest.java
@@ -0,0 +1,27 @@
+package seleniumIBMLearningArtifactId;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver", "C:\\geckodriverX64.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://google.com");
+		
+		WebElement element = driver.findElement(By.id("lst-ib"));
+		element.sendKeys("This is a test how SeleniumWebDriver works");
+		
+		element=driver.findElement(By.name("btnK"));
+		element.click();
+		
+		System.out.println("Test completed");
+
+
+	}
+
+}
+
