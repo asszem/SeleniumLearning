f4e5607313d2a47cc1802f0417d4e199f7ca7a61
Andras Olah <andras.antal.olah@hu.ibm.com>
2017-10-09 11:39:52 +0200

    Update FindByAnnotation with a Constructor that calls PageFactory

--- src/test/java/seleniumCommands/FindByAnnotation.java
+++ src/test/java/seleniumCommands/FindByAnnotation.java
@@ -11,35 +11,36 @@ public class FindByAnnotation {
 
 	static WebDriver driver;
 
-
 	public static void main(String[] args) {
 		System.out.println("FindBy Annotation test started");
 		driver = new FirefoxWebDriver().initializeFirefoxWebDriver();
 		driver.get("https://ibm.com");
-		
-		//PageFactory.initElements() must be used after page object instantiation to find elements marked with @FindBy annotation.
-		IBMMainPage ibmMainPage = new IBMMainPage();
-		PageFactory.initElements(driver, ibmMainPage);		
-		ibmMainPage.marketPlaceButton.click();
-	}
 
+		// PageFactory.initElements() must be used after page object instantiation to find elements marked with @FindBy annotation.
+		IBMMainPage ibmMainPage = new IBMMainPage(driver);
+//		ibmMainPage.marketPlaceButton.click();  //usind direct reference to webelement's click method
+		ibmMainPage.clickAtMarketPlaceButton(); //using the method in class
+	}
 }
 
-class IBMMainPage{
+class IBMMainPage {
 	// https://seleniumhq.github.io/selenium/docs/api/java/org/openqa/selenium/support/FindBy.html
 	// https://stackoverflow.com/documentation/selenium-webdriver/6777/using-findby-annotations-in-java#t=201710090845219559163
+	// https://stackoverflow.com/questions/18436102/selenium-findby-vs-driver-findelement
 	/*
-	CLASS_NAME: @FindBy(className = "classname")
-	CSS: @FindBy(css = "css")
-	ID: @FindBy(id = "id")
-	ID_OR_NAME: @FindBy(how = How.ID_OR_NAME, using ="idOrName")
-	LINK_TEXT: @FindBy(linkText= "text")
-	NAME: @FindBy(name= "name")
-	PARTIAL_LINK_TEXT: @FindBy(partialLinkText= "text")
-	TAG_NAME: @FindBy(tagName="tagname")
-	XPATH: @FindBy(xpath="xpath")
-	*/
+	 * CLASS_NAME: @FindBy(className = "classname") CSS: @FindBy(css = "css") ID: @FindBy(id = "id") ID_OR_NAME: @FindBy(how = How.ID_OR_NAME, using ="idOrName") LINK_TEXT: @FindBy(linkText= "text")
+	 * NAME: @FindBy(name= "name") PARTIAL_LINK_TEXT: @FindBy(partialLinkText= "text") TAG_NAME: @FindBy(tagName="tagname") XPATH: @FindBy(xpath="xpath")
+	 */
 	@FindBy(partialLinkText = "Marketplace")
 	static WebElement marketPlaceButton;
-	
+
+	// Constructor to initilase page object with PageFactory
+	public IBMMainPage(WebDriver driver) {
+		// PageFactory.initElements() must be used after page object instantiation to find elements marked with @FindBy annotation.
+		PageFactory.initElements(driver, this);
+	}
+
+	public void clickAtMarketPlaceButton(){
+		marketPlaceButton.click();
+	}
 }
