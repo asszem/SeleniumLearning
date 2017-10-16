1922150650974ad75769da4a18bb4baa8afc7c92
Andras Olah <andras.antal.olah@hu.ibm.com>
2017-10-16 10:07:39 +0200

    Update HandlingIframes with manipulating TestQA demo page iframes

--- src/test/java/seleniumCommands/HandlingIframes.java
+++ src/test/java/seleniumCommands/HandlingIframes.java
@@ -28,5 +28,28 @@ public class HandlingIframes {
 		// By finding all the web elements using iframe tag
 		List<WebElement> iframeElements = driver.findElements(By.tagName("iframe"));
 		System.out.println("The total number of iframes are " + iframeElements.size());
+		
+		// Displaying all iframe IDs
+		int totalIframes=iframeElements.size();
+		int currentIframeIndex=0;
+		for (WebElement element:iframeElements){
+			System.out.println(element.getAttribute("id")+" index="+currentIframeIndex++);
+		}
+		
+		//Switching to IF2
+		driver.switchTo().frame(iframeElements.get(1).getAttribute("id"));
+		
+		//Clicking to Tab2 on IF2 iframe
+		driver.findElement(By.id("ui-id-2")).click();
+
+		//Switching to IF1
+		driver.switchTo().defaultContent();
+		driver.switchTo().frame(iframeElements.get(0).getAttribute("id"));
+		
+		//Checking the QTP checbox
+		driver.findElement(By.id("tool-0")).click();
+
+		//Switching back to main iframe
+		driver.switchTo().defaultContent();
 	}
 }
